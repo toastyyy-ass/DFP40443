36e304ca39718f3a9657bff9387e19615c7dfffd
toasty <mnajhan2104@gmail.com>
2026-01-20 15:16:58 +0800

    third commit

--- W2/play.php
+++ W2/play.php
@@ -2,4 +2,7 @@
 $name = "najhan";
 echo "hello world $name";
 echo 'this is a full stack class'.$name;
+echo "<h1> my name is Muhammad Najhan</h1>";
+echo "<h2> im from JTMK</h2>";
+echo "<h1> and my PA is Miss Athirah.</h3>";
 ?>
